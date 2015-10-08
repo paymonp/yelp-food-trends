bbe0ee63bc949d6c3703a33f189b342a6ab25bb2
Paymon <paymon.p@berkeley.edu>
2015-10-07 23:33:43 -0700

    Added pizza filter

--- filter_reviews.py
+++ filter_reviews.py
@@ -1,39 +1,31 @@
 import json
 from utils import load_data
-import food_lib
-
-businesses = load_data("food_businesses.json")
-business_ids = set(food_lib.map_to_arg(food_lib.filter_by_city(businesses, "Las Vegas"), "business_id"))
-
-print("ok so starting loading")
-city = "Las Vegas"
-relevant_reviews = []
-#reviews = load_data("yelp_academic_dataset_review.json")
+from food_lib import *
 
 def filter_reviews():
+	businesses = load_data("food_businesses.json")
+	business_ids = set(food_lib.map_to_arg(food_lib.filter_by_city(businesses, "Las Vegas"), "business_id"))
 	out_file = open('vegas_reviews.json', 'w+')
 	with open('yelp_academic_dataset_review.json', 'r') as f:
 		for line in f:
 			line_json = json.loads(line)
 			if line_json['business_id'] in business_ids:
 				out_file.write(json.dumps(line_json) + '\n')
-filter_reviews()
-'''
 
 
 
-print("loaded reviews.")
-c = 0
-for r in reviews:
-	if r['business_id'] in business_ids:
-		reviews.append(json.dumps(r))
-	c += 1
-	if c % 10 == 0:
-		print('finished another 100000...')
-		print(len(reviews)-c, 'left to go')
+def process_categories():
+	reviews = load_data("vegas_reviews.json")
+	def pizza(line):
+		return 'Pizza' in line['categories']
+	vegas_cats = set(map_to_arg(load_data("food_businesses.json", pizza), 'business_id'))
+	times = []
+	for r in reviews:
+		if r['business_id'] in vegas_cats:
+			times.append(r['date'])
+	print(sorted(times))
 
-f = open("vegas_reviews.json", "w+")
-for r in reviews:
-	f.write(r) + "\n"
-f.close()
-'''
+
+process_categories()
+
+#filter_reviews()
