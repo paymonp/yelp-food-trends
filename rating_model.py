97fbd0835f092a40c64ee383d2e9817e3c30ab1b
Jeff <jeffdh5@gmail.com>
2015-10-21 23:36:04 -0700

    stuff

--- rating_model.py
+++ rating_model.py
@@ -1,6 +1,9 @@
 import json
 from utils import load_data
 from food_lib import *
+from sklearn import linear_model
+from sklearn.cross_validation import KFold
+import numpy as np 
 
 def featurize():
 	def pizza(line):
@@ -18,3 +21,32 @@ def featurize():
 	return labels, training_data
 
 labels, training_data = featurize()
+
+labels = np.array(labels)
+training_data = np.array(training_data)
+
+
+print(training_data.shape, labels.shape)
+
+def crossvalidate(X, y):
+	kf = KFold(X.shape[0], n_folds=10)
+	for train, test in kf:
+		Xtrain, ytrain = X[train], y[train]
+		Xtest, ytest = X[test], y[test]
+		model = linreg(Xtrain, ytrain)
+		score = model.score(Xtest, ytest)
+		print(score)
+
+
+
+
+def linreg(X, y):
+	model = linear_model.LinearRegression()
+	model.fit(X, y)
+	return model
+
+
+
+# ... load data somewhow
+
+#crossvalidate(X, y)
